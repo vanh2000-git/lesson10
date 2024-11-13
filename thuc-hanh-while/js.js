c94eb68c018289f3d1548705b6e2147f7a19a477
vanh2000-git <nguyenvietanh102000@gmail.com>
2024-11-13 13:41:22 +0700

    update all

---
+++ thuc-hanh-while/js.js
@@ -0,0 +1,15 @@
+// Bai 1
+while( num != -1 ) {
+    num = +prompt("Enter a number: ");
+    alert(number);
+    //phần code tính tổng
+    total += num;
+}
+// Bai 2
+let i = 1;
+
+while (i < 100) {
+    //phân thân vòng lặp
+    document.write("<hr width = " + i + "%>");
+    i++;
+}

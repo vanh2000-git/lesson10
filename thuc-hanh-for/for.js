aad5e66730bc00c8c5566e9623a5a44adce202c6
vanh2000-git <nguyenvietanh102000@gmail.com>
2024-11-13 11:05:31 +0700

    update

--- thuc-hanh-for/for.js
+++ thuc-hanh-for/for.js
@@ -4,3 +4,10 @@ for(i = 0; i <= 5; i++){
     text += "the number is" + i + "<br>";
 }
 document.getElementById("text").innerHTML = text;
+///
+let n= +prompt("nhập vào một số bất kỳ")
+let sum = 0;
+for (let j = 0; j < n; j++) {
+    sum += j;
+}
+alert(sum);
